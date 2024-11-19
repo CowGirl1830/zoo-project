ce6b534f25dd25ad7b67a349c3c34b85f0008e0c
Nawaphon Promnan <117667862+CowGirl1830@users.noreply.github.com>
2024-11-19 04:12:43 +0000

    "Edit zoo.py"

--- test_zoo.py
+++ test_zoo.py
@@ -11,19 +11,19 @@ class TestZoo(unittest.TestCase):
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
         self.assertEqual(self.zoo.get_ticket_price(0), 50)
-        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+        self.assertEqual(self.zoo.get_ticket_price(8), 50)
 
     def test_teen_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(5), 100)
         self.assertEqual(self.zoo.get_ticket_price(20), 100)
 
     def test_adult_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
         self.assertEqual(self.zoo.get_ticket_price(60), 150)
 
     def test_senior_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(61), 100)
-        self.assertEqual(self.zoo.get_ticket_price(70), 100)
+        self.assertEqual(self.zoo.get_ticket_price(69), 100)
 
 if __name__ == '__main__':
     unittest.main()
